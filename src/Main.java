c51d7e22dd17d1b63662481ad5c1340a36afa3e3
varil <varilyasovtimur@gmail.com>
2022-09-05 20:36:00 +0700

    Условный оператор. Урок 2 (done).

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,52 @@ public class Main {
         task5();
         task6();
         task7();
+        task8();
+
+    }
+
+    private static void task8() {
+        System.out.println("Задание 7.");
+
+        int age = 25;
+        int salary = 60_000;
+        int wantedSum = 330_000;
+
+        double bankPercent = 1.1; // Базовая ставка для клиента 10%
+        double bankPercentAge23 = 1.11; // ставка для клиента младше 23 лет 11%
+        double bankPercentAge30 = 1.105; // ставка для клиента младше 30 лет 10.5%
+        double bankPercentHigher80_000 = 1.093; // ставка для клиентов c зарплатой больше 80_000 9.3%
+
+        int loanTime = 12; // Срок кредитования 12 месяцев
+
+        double monthlyPayment = (wantedSum * bankPercent) / loanTime; // базовый ежемесячный платеж
+        double monthlyPaymentAgeBelow23 = (wantedSum * bankPercentAge23) / loanTime; // ежемесячный платеж для клиентов младше 23 лет
+        double monthlyPaymentAgeBelow30 = (wantedSum * bankPercentAge30) / loanTime; // ежемесячный платеж для клиентов младше 30 лет
+        double monthlyPaymentSalaryHigher80_000 = (wantedSum * bankPercentHigher80_000) / loanTime; // ежемесячный платеж для клиентов c зарплатой больше 80_000
+
+
+        double maximumLoanPayment = salary * 0.5; // Максимальный ежемесячный платеж 50% от ЗП
+
+        boolean ageBelow23 = age < 23; // возраст меньше 23, добавляем 1%
+        boolean ageBelow30 = age < 30; // возраст меньше 30, добавляем 0,5%
+        boolean salaryHigher80_000 = salary > 80_000; // зарплата больше 80_000, уменьшаем ставку на 0.7%
+
+        boolean comparisonAmountsOne = maximumLoanPayment > monthlyPaymentSalaryHigher80_000; // сравнение суммы для одобрения либо отказа
+
+
+
+        if (salaryHigher80_000){
+            if (comparisonAmountsOne){
+                System.out.printf("«Максимальный платеж при ЗП %d равен %.2f рублей." +
+                        " Платеж по кредиту %.2f рублей. Одобрено». \n", salary, maximumLoanPayment, monthlyPaymentSalaryHigher80_000);
+            }
+
+        }
+
+
+        // Вычислить максимальный платеж и платеж по кредиту.
+        //System.out.println("«Максимальный платеж при ЗП *** равен *** рублей. Платеж по кредиту *** рублей. Одобрено/отказано».");
+
 
 
     }
@@ -26,7 +72,6 @@ public class Main {
         System.out.print("Укажите доход клиента: "); // задаем суммы через консоль.
         int salary = two.nextInt();
 
-
         // допустимый лимит средств по возрасту
         double creditLimitOne = salary * 3;
         double creditLimitTwo = salary * 2;
@@ -38,21 +83,21 @@ public class Main {
         double creditLimitPercentFour = creditLimitTwo * 1.5;
 
         if (age >= 23) {
-            if (salary >= 50 && salary < 80) {
-                System.out.printf("«Мы готовы выдать вам кредитную карту с лимитом %.2F рублей». \n", creditLimitPercentOne);
-            } else if (salary >= 80) {
-                System.out.printf("«Мы готовы выдать вам кредитную карту с лимитом %.2F рублей». \n", creditLimitPercentTwo);
+            if (salary < 50_000) {
+                System.out.printf("«Мы готовы выдать вам кредитную карту с лимитом %.2f рублей». \n", creditLimitOne);
+            } else if (salary < 80_000) {
+                System.out.printf("«Мы готовы выдать вам кредитную карту с лимитом %.2f рублей». \n", creditLimitPercentOne);
             } else {
-                System.out.printf("«Мы готовы выдать вам кредитную карту с лимитом %.2F рублей». \n", creditLimitOne);
+                System.out.printf("«Мы готовы выдать вам кредитную карту с лимитом %.2f рублей». \n", creditLimitPercentTwo);
             }
         }
         if (age < 23) {
-            if (salary >= 50 && salary < 80) {
-                System.out.printf("«Мы готовы выдать вам кредитную карту с лимитом %.2F рублей». \n", creditLimitPercentThree);
-            } else if (salary >= 80) {
-                System.out.printf("«Мы готовы выдать вам кредитную карту с лимитом %.2F рублей». \n", creditLimitPercentFour);
+            if (salary < 50_000) {
+                System.out.printf("«Мы готовы выдать вам кредитную карту с лимитом %.2f рублей». \n", creditLimitTwo);
+            } else if (salary < 80_000) {
+                System.out.printf("«Мы готовы выдать вам кредитную карту с лимитом %.2f рублей». \n", creditLimitPercentThree);
             } else {
-                System.out.printf("«Мы готовы выдать вам кредитную карту с лимитом %.2F рублей». \n", creditLimitTwo);
+                System.out.printf("«Мы готовы выдать вам кредитную карту с лимитом %.2f рублей». \n", creditLimitPercentFour);
             }
         }
     }
@@ -109,14 +154,18 @@ public class Main {
     private static void task5() {
         System.out.println("Задание 4.");
 
-        int deliveryDistance = 95; // дистанцию до клиента
+        Scanner one = new Scanner(System.in );
+        System.out.print("Укажите дистанцию до клиента в км: "); // задаем суммы через консоль.
+        int deliveryDistance = one.nextInt();
+
+        //int deliveryDistance = 95; // дистанцию до клиента
 
         if (deliveryDistance <= 20){
-            System.out.println("Потребуется дней: " + " 1");
-        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
-            System.out.println("Потребуется дней: " + " 2");
-        } else if (deliveryDistance > 60 && deliveryDistance <= 100){
-            System.out.println("Потребуется дней: " + " 3");
+            System.out.println("Потребуется дней: " + "1");
+        } else if (deliveryDistance <= 60) {
+            System.out.println("Потребуется дней: " + "2");
+        } else if (deliveryDistance <= 100){
+            System.out.println("Потребуется дней: " + "3");
         } else {
             System.out.println("Доставки не будет"); }
     }
@@ -130,21 +179,21 @@ public class Main {
 
             // Шаг 1. Узнать делится ли год на 4 без остатка.
             int stepOne = year % 4; // делим год на 4
-            boolean remainsDivideOne = stepOne == 0; // выясняем остаток равен 0 или нет.
+            boolean remainsDivideZeroOne = stepOne == 0; // выясняем остаток равен 0 или нет.
 
             // Шаг 2. Узнать делится ли год на 100 без остатка
             int stepTwo = year % 100; // делим год на 100
-            boolean remainsDivideTwo = stepTwo == 0; // выясняем остаток равен 0 или нет.
+            boolean remainsDivideZeroTwo = stepTwo == 0; // выясняем остаток равен 0 или нет.
 
             // Шаг 3. Узнать делится ли год на 400 без остатка
             int stepThree = year % 400; // делим год на 400
-            boolean remainsDivideThree = stepThree == 0; // выясняем остаток равен 0 или нет.
+            boolean remainsDivideZeroThree = stepThree == 0; // выясняем остаток равен 0 или нет.
 
-            if (remainsDivideOne == false) { // step 1
+            if (!remainsDivideZeroOne) { // step 1
                 System.out.printf("%d год не является високосным \n", year);
-            } else if (remainsDivideTwo == false) { // step 2
+            } else if (!remainsDivideZeroTwo) { // step 2
                 System.out.printf("%d год является високосным \n", year);
-            } else if (remainsDivideThree == false) { //step 3
+            } else if (!remainsDivideZeroThree) { //step 3
                 System.out.printf("%d год не является високосным \n", year);
             } else { // step 4
                 System.out.printf("%d год является високосным \n", year);
@@ -172,10 +221,10 @@ public class Main {
             int max = 2022; // диапазон значений
             int diff = max - min;
             int randomPhoneReleaseYear = random.nextInt(diff + 1) + min; //год выпуска телефона
-            boolean comparisonPhoneReleaseYear = clientDeviceYear < randomPhoneReleaseYear; // сравнение года выпуска с маркером данных.
+            boolean yearOfRelease2015 = clientDeviceYear < randomPhoneReleaseYear; // сравнение года выпуска с маркером данных.
 
             if (clientOS == 0) {
-                if (comparisonPhoneReleaseYear == true) {
+                if (yearOfRelease2015) {
                 System.out.println("«Установите версию приложения для iOS по ссылке».");
                 } else {
                 System.out.println("«Установите облегченную версию приложения для iOS по ссылке».");
@@ -183,7 +232,7 @@ public class Main {
             }
 
             if (clientOS != 0) {
-                if (comparisonPhoneReleaseYear == false) {
+                if (!yearOfRelease2015) {
                     System.out.println("«Установите облегченную версию приложения для Android по ссылке».");
                 } else {
                     System.out.println("«Установите версию приложения для Android по ссылке».");
